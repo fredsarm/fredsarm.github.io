c876878f33724b23bfeb38bf5e5c22acbe56a295
fredsarm <fredsarm.js@gmail.com>
2022-08-03 19:51:33 -0300

    reengineer, put articles in embeded element

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,57 @@
- 
+function fade(id,interval) {
+    let i = 0;
+    let embArt = document.getElementById(id);
+    embArt.style.opacity = 0;
+    let k = window.setInterval(function() {
+    if (i >= 10) {
+        clearInterval(k);
+    } else {
+        embArt.style.opacity = i / 10;
+        i++;
+    }
+    }, interval);
+};
+window.onload = function () { 
+    fade("tudo",200);
+    fade("article-embeded",30);
+    let embedSources = {
+        home: {
+            id:"logo",
+            src: "./articles/home.html"
+        },
+        projects: {
+            id:"link-projects",
+            src:"./articles/projects.html"
+        },
+        bio: {
+            id:"link-bio",
+            src: "./articles/bio.html"
+        },
+        contact: {
+            id: "link-contact",
+            src: "./articles/contact.html"
+        },
+        skills: {
+            id: "link-skils",
+            src:"./articles/skills.html"
+        },
+        technologies: {
+            id: "link-technologies",
+            src: "./articles/technologies.html"
+        },
+        report: {
+            id: "link-report",
+            src: "./articles/report.html"
+        } 
+    }
+    for (let menu in embedSources) {
+        let varId=embedSources[menu]["id"];
+        let varSrc=embedSources[menu]["src"];
+        let changeSrc = () => {
+            document.getElementById("article-embeded").style.opacity=0;
+            document.getElementById("article-embeded").setAttribute("src", varSrc);
+            fade("article-embeded",30)
+        }
+        document.getElementById(varId).addEventListener("click", changeSrc);
+    }
+}
