ee4dd6ee21dc82caddd797c218a5d8056d955723
fredsarm <fredsarm.js@gmail.com>
2022-08-04 07:00:31 -0300

    Alter timing of fade out

--- javascript/main.js
+++ javascript/main.js
@@ -50,7 +50,7 @@ window.onload = function () {
         let changeSrc = () => {
             document.getElementById("article-embeded").style.opacity=0;
             document.getElementById("article-embeded").setAttribute("src", varSrc);
-            fade("article-embeded",30)
+            fade("article-embeded",60)
         }
         document.getElementById(varId).addEventListener("click", changeSrc);
     }
